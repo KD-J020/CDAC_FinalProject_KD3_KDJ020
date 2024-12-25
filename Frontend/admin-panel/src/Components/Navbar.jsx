704794b358f37ea3139acb1bd981fb78c4d40924
mayur-v-p <mayurvpawar8@gmail.com>
2024-12-25 18:04:32 +0530

    Admin_Navbar

---
+++ Frontend/admin-panel/src/Components/Navbar.jsx
@@ -0,0 +1,82 @@
+import { Link } from "react-router-dom";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+function Navbar() {
+  return (
+    <>
+      <nav
+        className="navbar navbar-expand-lg navbar-light "
+        style={{ backgroundColor: "#438a84" }}
+      >
+        <div className="container-fluid w-100">
+          <a className="navbar-brand" href="#">
+            CRM
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to={""}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to={""}>
+                  About
+                </Link>
+              </li>
+
+              <li className="nav-item dropdown mr-4">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Profile
+                </Link>
+                <ul className="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Edit Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Logout
+                    </Link>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Something else here
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
