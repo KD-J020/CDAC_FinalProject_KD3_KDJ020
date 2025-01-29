723be8d16b4895145b877d5cc8fcff163e190451
mayur-v-p <mayurvpawar8@gmail.com>
2025-01-29 12:35:30 +0530

    User Backend Api

--- Backend/Admin-Panel/src/main/java/com/cdac/project/service/UserService.java
+++ Backend/Admin-Panel/src/main/java/com/cdac/project/service/UserService.java
@@ -1,9 +1,33 @@
 package com.cdac.project.service;
 
+import java.util.List;
+
+import com.cdac.project.dto.ApiResponse;
 import com.cdac.project.dto.UserDto;
+import com.cdac.project.dto.UserResponseDto;
 
 public interface UserService {
 
-	String createUser(UserDto user);
+	ApiResponse createUser(UserDto user);
+
+	List<UserResponseDto> getAllUsers();
+
+	UserResponseDto getUserById(Long userId);
+
+	ApiResponse updateUser(Long userId, UserDto userDto);
+
+	ApiResponse deleteUser(Long userId);
+
+	List<UserResponseDto> getAllCustomers();
+
+	List<UserResponseDto> getAllExecutive();
+
+	List<UserResponseDto> getAllActiveCustomers();
+
+	List<UserResponseDto> getAllInActiveCustomers();
+
+	List<UserResponseDto> getAllActiveExecutive();
+
+	List<UserResponseDto> getAllInActiveExecutive();
 
 }
