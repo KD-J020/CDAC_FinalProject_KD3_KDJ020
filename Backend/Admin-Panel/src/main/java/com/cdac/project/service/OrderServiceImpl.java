5e518bc219789539a29f013bfddba99e4b65bfe4
Omkar Bhise <omkar.bhise74@gmail.com>
2025-01-30 18:43:58 +0530

    order entity and order controller added

---
+++ Backend/Admin-Panel/src/main/java/com/cdac/project/service/OrderServiceImpl.java
@@ -0,0 +1,43 @@
+package com.cdac.project.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cdac.project.custom_exception.ResourceNotFoundException;
+import com.cdac.project.dto.OrderResponseDto;
+
+import com.cdac.project.entity.User;
+import com.cdac.project.repository.OrderRepository;
+import com.cdac.project.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class OrderServiceImpl implements OrderService{
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Override
+	public List<OrderResponseDto> getAllOrdersByCustomerId(Long customerId) {
+		User user  = userRepository.findById(customerId).orElseThrow();
+		if(user != null) {
+			return 	orderRepository.findAllByUserId(customerId)
+					.stream()
+					.map(order -> modelMapper.map(order, OrderResponseDto.class))
+					.collect(Collectors.toList());
+		} else {
+			throw new ResourceNotFoundException("Invalid Customer ID !!!!!!!!");
+		}
+	}
+}
