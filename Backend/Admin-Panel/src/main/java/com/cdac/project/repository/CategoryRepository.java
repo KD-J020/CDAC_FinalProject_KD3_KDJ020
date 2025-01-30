09f4a98442d56b0186f93ce9f9cefff3bc2589e8
Omkar <toraseomkar@gmail.com>
2025-01-30 11:27:57 +0530

    Category Api

---
+++ Backend/Admin-Panel/src/main/java/com/cdac/project/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.cdac.project.repository;
+
+import com.cdac.project.entity.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
---
+++ Backend/Admin-Panel/src/main/java/com/cdac/project/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.cdac.project.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.cdac.project.dto.ApiResponse;
+import com.cdac.project.dto.CategoryDto;
+
+import jakarta.transaction.Transactional;
+@Service
+@Transactional
+public interface CategoryService {
+
+	public ApiResponse addCategory(CategoryDto dto);
+
+	public List<CategoryDto> getAllCategory();
+
+	public CategoryDto getCategoryById(Long id);
+
+}
