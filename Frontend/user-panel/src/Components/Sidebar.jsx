f3ec615dca31d363e5ee7fa8ee92bd893d90ea18
Omkar <toraseomkar@gmail.com>
2025-02-09 13:51:05 +0530

    product changed

--- Frontend/user-panel/src/Components/Sidebar.jsx
+++ Frontend/user-panel/src/Components/Sidebar.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import {Link} from "react-router-dom"
-import { Dropdown} from 'react-bootstrap';
-import "../Styles/sidebar.css"; 
+import { Dropdown } from "react-bootstrap";
+import "../Styles/sidebar.css";
 
 function Sidebar() {
   return (
     <div className="d-flex flex-column vh-100 text-white p-3" style={{ width: "180px", backgroundColor: "#438a84" }}>
       <ul className="nav nav-pills flex-column">
-        <li className="nav-item">
-          <Link to="/home/Product" className="nav-link text-white">Product Details</Link>
-        </li>
+        
         <li className="nav-item">
           <Link to="/home/NewTicket" className="nav-link text-white">Raise Ticket</Link>
         </li>
@@ -31,24 +27,26 @@ function Sidebar() {
         </li>
         <li className="nav-item">
           <Link to="/home/add-feedback" className="nav-link text-white">Submit Feedback</Link>
+        </li>  {/* ✅ Fixed: Added missing closing tag */}
 
+        {/* History Dropdown */}
         <li className="nav-item dropdown">
           <Dropdown>
-              <Dropdown.Toggle
-                id="historyDropdown"
-                className="nav-link dropdown-toggle text-white bg-transparent border-0"
-              >
-                History
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item as={Link} to="/home/History/Purchases">
-                  Purchases
-                </Dropdown.Item>
-                <Dropdown.Item as={Link} to="/home/History/Tickets">
-                  Tickets
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+            <Dropdown.Toggle
+              id="historyDropdown"
+              className="nav-link dropdown-toggle text-white bg-transparent border-0"
+            >
+              History
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item as={Link} to="/home/History/Purchases">
+                Purchases
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/home/History/Tickets">
+                Tickets
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </li>
       </ul>
     </div>
